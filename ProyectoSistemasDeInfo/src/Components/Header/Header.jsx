185a610e576bc5c365886be369797f9168163f52
Franco <franco.lattanzio@correo.unimet.edu.ve>
2024-06-21 01:06:33 -0400

    header hecho

--- ProyectoSistemasDeInfo/src/Components/Header/Header.jsx
+++ ProyectoSistemasDeInfo/src/Components/Header/Header.jsx
@@ -1,7 +1,21 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import logo from '../../assets/LogoHeader.png';
+import { Link } from 'react-router-dom';
 
 function Header(){
-
+  return (
+    <header className={styles.header}>
+      <Link to="/Landing" className={styles.logoContainer}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </Link>
+      <nav className={styles.nav}>
+        <Link to="/Landing" className={styles.navItem}>Inicio</Link>
+        <Link to="/Menu" className={styles.navItem}>Menú</Link>
+        <Link to="/Nosotros" href="#nosotros" className={styles.navItem}>Nosotros</Link>
+        <Link to="/InicioDeSesion" href="#acceder" className={styles.button}>Acceder</Link>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
