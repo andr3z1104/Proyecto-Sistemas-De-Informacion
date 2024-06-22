3cd073639ed5d10f7020f6bd4301f18f77691e0f
vickysaldivia <vsaldivia@correo.unimet.edu.ve>
2024-06-22 09:51:34 -0400

    Carouseles listos (creo)

--- ProyectoSistemasDeInfo/src/Components/ProductCarousel/ProductCarousel.jsx
+++ ProyectoSistemasDeInfo/src/Components/ProductCarousel/ProductCarousel.jsx
@@ -9,51 +9,70 @@ import image3 from '../../assets/CaramelLatte.png';
 import image4 from '../../assets/Mocca.png';
 import image5 from '../../assets/Macchiato.png';
 
-{/**Son librerias que permiten hacer un carousel horizontal */}
 
-function ProductCarousel(){
+
+// Flechas para ver los demas productos
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`${className} ${styles.arrow}`}
+            style={{ ...style, right: "10px" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`${className} ${styles.arrow}`}
+            style={{ ...style, left: "10px" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function ProductCarousel() {
     const productos = [
         { id: 1, name: 'AMERICANO', price: '$3.5', image: image1 },
         { id: 2, name: 'CAPUCCINO', price: '$3.5', image: image2 },
         { id: 3, name: 'CARAMEL LATTE', price: '$5.99', image: image3 },
         { id: 4, name: 'MOCCA', price: '$3.5', image: image4 },
         { id: 5, name: 'EXPRESSO', price: '$4.99', image: image5 },
-    ]
+    ];
 
     const config = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1};
-
-
-    const Prev = (props) => {
-        
-    }
-
-    const productosCarousel = [];
-    for(let i = 0; i < productos.length; i++){
-        const producto = productos[i];
-        productosCarousel.push(
-            <div key={producto.id} className={styles.tarjetaProductos}>
-                <img src={producto.image} alt={producto.name} />
-                <h3>{producto.name}</h3>
-                <p>{producto.price}</p>
-                <button className={styles.navButton}>Ver Menú</button>
-            </div>
-        )
-    }
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: true,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    };
 
     return (
-        <div>
-            <Slider{...config}>
-            {productosCarousel}
+        <div className={styles.Slider}>
+            <p className={styles.Header}>Calidad a tu alcance</p>
+            <Slider {...config}>
+                {productos.map((producto) => (
+                    <div key={producto.id} className={styles.tarjetaProductos}>
+                        <div className={styles.centerContent}>
+                            <img src={producto.image} alt={producto.name} className={styles.img} />
+                        </div>
+                        <div className={styles.textContainer}>
+                            <p className={styles.TextName}>{producto.name}</p>
+                            <p className={styles.TextPrice}>{producto.price}</p>
+                        </div>
+                        <button className={styles.navButton}>Menú</button>
+                    </div>
+                ))}
             </Slider>
         </div>
-    )
-
-
+    );
 }
 
-export default ProductCarousel
+export default ProductCarousel;
