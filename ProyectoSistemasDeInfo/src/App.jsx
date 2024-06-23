8546fde9bb230d9c63a457e3c170ba6ea6b645d1
andr3z1104 <moreno.andres@correo.unimet.edu.ve>
2024-06-23 11:21:51 -0400

    Pagina de Registrarse

--- ProyectoSistemasDeInfo/src/App.jsx
+++ ProyectoSistemasDeInfo/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './Pages/Landing/Landing';
 import InicioDeSesion from './Pages/InicioDeSesion/InicioDeSesion';
+import Registrarse from './Pages/Registrarse/Registrarse'
 import Contacto from './Pages/Contacto/Contacto';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
@@ -16,7 +17,8 @@ function App() {
         <Header />
         <Routes>
           <Route path='/Landing' element={<Landing />} />
-          <Route path='/InicioDeSesion' element={<InicioDeSesion />} /> 
+          <Route path='/InicioDeSesion' element={<InicioDeSesion/>} /> 
+          <Route path='/Registrarse' element={<Registrarse/>} />
           <Route path='/Contacto' element={<Contacto />} />
         </Routes>
         <Footer />
