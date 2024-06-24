39e52cc1a351259fb1c7d772246db83b6408de22
Giuseppe Vaccaro <gvaccaro@correo.unimet.edu.ve>
2024-06-24 03:54:39 -0400

    Css actualización
    Modifiqué algunas estructuras que ustedes estaban utilizando en el jsx al igual que rehice la parte del css de
    landing
    Header
    Contacto (No fue rehecha en un 100%)
    
    Las que pude arreglar sin rehacer el código fue Footer

--- ProyectoSistemasDeInfo/src/Components/Header/Header.jsx
+++ ProyectoSistemasDeInfo/src/Components/Header/Header.jsx
@@ -11,19 +11,23 @@ function Header(){
 
   return (
     <header className={styles.header}>
-      <Link to="/" className={styles.logoContainer}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-      </Link>
-      <nav className={styles.nav}>
-        <div className={styles.parte1}>
-          <Link to="/" className={styles.navItem}>Inicio</Link>
-          <a href="/Menu" className={styles.navItem} onClick={onClick}>Menú</a>
-          <a href="/Nosotros" className={styles.navItem} onClick={onClick}>Nosotros</a>
-        </div>
-        <div className={styles.parte2}>
-          <Link to="/InicioDeSesion" href="#acceder" className={styles.button}>Acceder</Link>
-        </div>
-      </nav>
+      
+        <Link to="/" className={styles.logoContainer}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+        </Link>
+    
+      
+        <nav className={styles.nav}>
+          <div className={styles.parte1}>
+            <Link to="/" className={styles.navItem}>Inicio</Link>
+            <a href="/Menu" className={styles.navItem} onClick={onClick}>Menú</a>
+            <a href="/Nosotros" className={styles.navItem} onClick={onClick}>Nosotros</a>
+          </div>
+          <div className={styles.parte2}>
+            <Link to="/InicioDeSesion" href="#acceder" className={styles.button}>Acceder</Link>
+          </div>
+        </nav>
+      
     </header>
   );
 }
