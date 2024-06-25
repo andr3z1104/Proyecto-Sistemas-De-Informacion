12d2ee93a012d6269e53bba4680d07b2afbe731e
vickysaldivia <vsaldivia@correo.unimet.edu.ve>
2024-06-24 22:43:09 -0400

    Final Commit

--- src/Components/ProductCarousel/ProductCarousel.jsx
+++ src/Components/ProductCarousel/ProductCarousel.jsx
@@ -62,7 +62,7 @@ function ProductCarousel() {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                 }
             },
