0812a39a7bd32721e519d0039a45690354e070a7
andr3z1104 <moreno.andres@correo.unimet.edu.ve>
2024-06-30 15:32:07 -0400

    Carruseles Menu

---
+++ src/appData.js
@@ -0,0 +1,32 @@
+
+const dataProducts = [
+    {
+        ID: 1,
+        nombre: 'Cafe Americano',
+        categoría: 'Cafe',
+        descripción: 'Un cafe tostado para empezar el dia',
+        precio: 1,
+        cantidad: 10,
+        img: '/src/assets/Americano.png'
+    },
+    {
+        ID: 2,
+        nombre: 'Pan Integral',
+        categoría: 'Pan',
+        descripción: 'Un pancito para empezar el dia',
+        precio: 2,
+        cantidad: 20,
+        img: '/src/assets/pan.png'
+    },
+    {
+        ID: 3,
+        nombre: 'Frappe Chocolate',
+        categoría: 'Frappe',
+        descripción: 'Un frappe bien frio para terminar el dia',
+        precio: 3,
+        cantidad: 10,
+        img: '/src/assets/FrappeChocolate.png'
+    },
+]
+
+export default dataProducts
