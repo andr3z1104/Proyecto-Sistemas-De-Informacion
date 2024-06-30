a562c0d2a475cd87ecaacbbc3fd88bd062e29713
Giuseppe Vaccaro <gvaccaro@correo.unimet.edu.ve>
2024-06-30 09:56:13 -0400

    Listo cambios pendientes
    Css de
    Footer, header, image carousel, product carousel todavía en espera.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import styles from './Footer.module.css'
 import { Link } from 'react-router-dom';
+import global from "../../Global.module.css"
 
 
 function Footer(){
@@ -7,8 +8,8 @@ function Footer(){
     <footer className={styles.footer}>
         <div className={styles.img}>
             <nav className={styles.nav}>
-                <a href='https://maps.app.goo.gl/GvuckTwh5J6fcL6m7' className={styles.navItem}>VISÍTANOS</a>
-                <Link to='/Contacto' className={styles.navItem}>CONTÁCTANOS</Link>
+                <a href='https://maps.app.goo.gl/GvuckTwh5J6fcL6m7' className={`${styles.navItem} ${global.boton}`}>VISÍTANOS</a>
+                <Link to='/Contacto' className={`${styles.navItem} ${global.boton}`}>CONTÁCTANOS</Link>
             </nav>
         </div>
     </footer>
--- src/Components/ProductCarousel/ProductCarousel.jsx
+++ src/Components/ProductCarousel/ProductCarousel.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/jsx-key */
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './custom-slick.css'; // Importar el archivo CSS personalizado
+
+
 import styles from './ProductCarousel.module.css';
+import global from "../../Global.module.css"
 
 import image1 from '../../assets/Americano.png';
 import image2 from '../../assets/Capuccino.png';
@@ -14,32 +19,33 @@ import image8 from '../../assets/OmeleteDeVegetales.png';
 
 
 
-// Flechas para ver los demas productos
+//Flechas para ver los demas productos
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={`${className} ${styles.arrow}`}
-            style={{ ...style, right: "10px" }}
-            onClick={onClick}
+     const { className, style, onClick } = props;
+     return (
+         <div
+             className={`${className} ${styles.arrow}`}
+             style={{ ...style, right: "10px" }}
+             onClick={onClick}
         />
     );
 }
 
 function SamplePrevArrow(props) {
     const { className, style, onClick } = props;
-    return (
-        <div
-            className={`${className} ${styles.arrow}`}
-            style={{ ...style, left: "10px" }}
-            onClick={onClick}
+     return (
+         <div
+             className={`${className} ${styles.arrow}`}
+             style={{ ...style, left: "10px" }}
+             onClick={onClick}  
+              
         />
-    );
+     );
 }
 
 function ProductCarousel() {
     const productos = [
-        { id: 1, name: 'CAFE AMERICANO', price: '$3.5', image: image1 },
+        { id: 1, name: 'CAFE AMERICANO', price: '$3.5', image: image1},
         { id: 2, name: 'CAFE CAPUCCINO', price: '$3.5', image: image2 },
         { id: 3, name: 'CAFE CARAMEL LATTE', price: '$5.99', image: image3 },
         { id: 4, name: 'CAFE MOCCA', price: '$3.5', image: image4 },
@@ -48,54 +54,75 @@ function ProductCarousel() {
         { id: 7, name: 'DESAYUNO AMERICANO', price: '$8.00', image: image7 },
         { id: 8, name: 'OMELET DE VEGETALES', price: '$7.00', image: image8 },
     ];
-
-    const config = {
+    
+    var settings = {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        centerMode: true,
+        lazyLoad: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        slidesMargin: 10,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true,
+              dots: false
             }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
         ]
-    };
+      };
 
+   
     return (
-        <div className={styles.Slider}>
-            <p className={styles.Header}>Calidad a tu alcance</p>
-            <div className={styles.contenedorTarjeta}>
-                <Slider {...config}>
-                    {productos.map((producto) => (
-                        <div key={producto.id} className={styles.tarjetaProductos}>
-                            <div className={styles.centerContent}>
-                                <img src={producto.image} alt={producto.name} className={styles.img} />
+        
+        <div className={styles.contenedorCarrusel}>
+            <p className={styles.tituloCarrusel}>Calidad a tu alcance</p>
+            <div className={styles.tarjetaCarrusel}>
+                <Slider {...settings}>
+                    {productos.map( (d) => (
+                        <div className={styles.contenedor}>
+                            
+                            <div className={styles.contenedorImagen}>
+                                <img className={styles.imagen} src={d.image} alt="opcionMenú" />
                             </div>
-                            <div className={styles.textContainer}>
-                                <p className={styles.TextName}>{producto.name}</p>
-                                <p className={styles.TextPrice}>{producto.price}</p>
+
+                            <div className={styles.descripcion}>
+                                <p className={styles.descripcionNombre}>{d.name} </p>
+                                <p className={styles.descripcionPrecio}>{d.price} </p>
+                                <button className={`${styles.descripcionBoton} ${global.boton}`}>Menú</button>
+                                
                             </div>
-                            <button className={styles.navButton}>Menú</button>
+                            
                         </div>
+
                     ))}
+
                 </Slider>
             </div>
         </div>
+
     );
 }
 
