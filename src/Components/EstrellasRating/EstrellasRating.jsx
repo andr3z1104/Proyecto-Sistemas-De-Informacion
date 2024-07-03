c53aecc820cc009112eb038e9fa8af7aa298cdfd
vickysaldivia <vsaldivia@correo.unimet.edu.ve>
2024-07-03 11:35:40 -0400

    Feedback interfaz y medio funcionalidad hecha

--- src/Components/EstrellasRating/EstrellasRating.jsx
+++ src/Components/EstrellasRating/EstrellasRating.jsx
@@ -1,47 +1,41 @@
-
-import { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import styles from "./EstrellasRating.module.css";
+import { useState } from 'react';
 
-const StarRating = () => {
-    const [rating, setRating] = useState(0);
+const StarRating = ({ rating, setRating }) => {
     const [hover, setHover] = useState(null);
 
     return (
-    <div className={styles.starrating}>
-        <div className={styles.star}>
-        {[...Array(5)].map((star, index) => {
-            const ratingValue = index + 1;
-            return (
-            <label key={index}>
+        <div className={styles.starrating}>
+            <div className={styles.star}>
+                {[...Array(5)].map((star, index) => {
+                    const ratingValue = index + 1;
+                    return (
+                        <label key={index}>
+                            <input
+                                type="radio"
+                                name="rating"
+                                value={ratingValue}
+                                onClick={() => setRating(ratingValue)}
+                                style={{ display: 'none' }}
+                            />
+                            <FaStar
+                                size={50}
+                                color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
+                                onMouseEnter={() => setHover(ratingValue)}
+                                onMouseLeave={() => setHover(null)}
+                                style={{ cursor: 'pointer', transition: 'color 200ms' }}
+                            />
+                        </label>
+                    );
+                })}
+            </div>
 
-                <input
-                type="radio"
-                name="rating"
-                value={ratingValue}
-                onClick={() => setRating(ratingValue)}
-                style={{ display: 'none' }}
-                />
-                
-                <FaStar
-                size={50}
-                color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
-                onMouseEnter={() => setHover(ratingValue)}
-                onMouseLeave={() => setHover(null)}
-                style={{ cursor: 'pointer', transition: 'color 200ms' }}
-                />
-
-            </label>
-            );
-        })}
+            <div className={styles.Text}>
+                <span>Pésima</span>
+                <span>Excelente</span>
+            </div>
         </div>
-
-        <div className={styles.Text}>
-            <span>Pésima</span>
-            <span>Excelente</span>
-        </div>
-        
-    </div>
     );
 };
 
