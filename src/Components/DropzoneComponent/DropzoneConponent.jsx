f65e88bfb1dd5c27669f460843692b38057b7bf7
Franco <franco.lattanzio@correo.unimet.edu.ve>
2024-07-04 00:12:56 -0400

    prueba3

--- src/Components/DropzoneComponent/DropzoneConponent.jsx
+++ src/Components/DropzoneComponent/DropzoneConponent.jsx
@@ -2,18 +2,17 @@ import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import styles from './DropzoneConponent.module.css';
 
-
 function DropzoneComponent() {
   const onDrop = useCallback((acceptedFiles) => {
     console.log(acceptedFiles);
   }, []);
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, open } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className={styles.dropzone}>
       <input {...getInputProps()} />
-      <div className={styles.text}>Añadir imagen</div>
+      <div className={styles.text} onClick={open}>Añadir imagen</div>
     </div>
   );
 }
