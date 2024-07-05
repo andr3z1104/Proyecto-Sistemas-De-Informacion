6708e7110b1e912dba70912bb9c49cb31bd0b820
VickySaldivia <144860192+vickysaldivia@users.noreply.github.com>
2024-07-05 16:22:00 -0400

    Update Function Cart :white_check_mark:

--- src/Components/CaruselMenu/CaruselMenu.jsx
+++ src/Components/CaruselMenu/CaruselMenu.jsx
@@ -1,119 +1,137 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { Link } from 'react-router-dom'; 
-import { useContext } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
 
-import { DataContext } from '../../Context/DataProvider';
+import { DataContext } from "../../Context/DataProvider";
+import { useUser } from "../../Controllers/UserContext";
+import useCart from "../../hooks/useCart";
+import { createOrUpdateCart } from "../../Controllers/cartController";
 
-import styles from './CaruselMenu.module.css';
-import global from "../../Global.module.css"
+import styles from "./CaruselMenu.module.css";
+import global from "../../Global.module.css";
 
-//import dataProducts from '../../appData'
-
-
-//Flechas para ver los demas productos
+// Flechas para ver los demás productos
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={`${className} ${styles.arrow}`}
-            style={{ ...style, right: "10px" }}
-            onClick={onClick}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} ${styles.arrow}`}
+      style={{ ...style, right: "10px" }}
+      onClick={onClick}
+    />
+  );
 }
-
 
 function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={`${className} ${styles.arrow}`}
-            style={{ ...style, left: "10px" }}
-            onClick={onClick}    
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} ${styles.arrow}`}
+      style={{ ...style, left: "10px" }}
+      onClick={onClick}
+    />
+  );
 }
 
+function CaruselMenu({ titulo }) {
+  const value = useContext(DataContext);
+  const productos = value.productos;
+  const { user } = useUser();
+  const cart = useCart();
 
+  const handleAddToCart = async (product) => {
+    console.log("User data:", user);
+    console.log("Product data:", product);
 
-function caruselMenu({titulo}) {
+    if (user) {
+      if (product && product.ID && product.precio) {
+        await createOrUpdateCart(user, product);
+      } else {
+        console.error("Product data is incomplete:", product);
+      }
+    } else {
+      alert("Por favor, inicie sesión para añadir productos al carrito");
+    }
+  };
 
-    const value = useContext(DataContext);
-
-    const productos = value.productos
-    const [carrito, setCarrito] = value.carrito;
-
-    const addCarrito = value.addCarrito
-
-
-    var settings = {
-        adaptiveHeight: true,
-        dots: false,
-        infinite: true,
-        speed: 500,
-        lazyLoad: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />,
-        responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            infinite: false,
-            dots: false
-            }
+  var settings = {
+    adaptiveHeight: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    lazyLoad: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false,
         },
-        {
-            breakpoint: 600,
-            settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            initialSlide: 1
-
-            }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
         },
-        {
-            breakpoint: 480,
-            settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-            }
-        }
-        ]
-    };
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-    return (
-        
-        <div className={styles.contenedorCarrusel}>
-            <p className={styles.tituloCarrusel}>{titulo}</p>
-            <div className={styles.tarjetaCarrusel}>
-                <Slider {...settings}>
-                {productos.filter((d) => d.categoria === titulo).map((d) => (
-                        <div className={styles.contenedor}>
-                            <Link to={`/ProductoDetalles/${d.ID}`} className={styles.contenedorImagen}>
-                                <img className={styles.imagen} src={d.img} alt={d.nombre} />
-                            </Link>
-                            <div className={styles.descripcion}>
-                                <p className={styles.descripcionNombre}>{d.nombre} </p>
-                                <p className={styles.descripcionPrecio}>{d.precio} $</p>
-                                <Link to={`/ProductoDetalles/${d.ID}`} className={`${styles.descripcionBoton} ${global.boton}`}>Ver Detalles </Link>
-                                <button id={d.ID} className={`${styles.descripcionBoton} ${global.boton}`} onClick={() => addCarrito(d.ID)} >Añadir Carrito</button>
-                            </div>
-                            
-                        </div>
-                    ))}
-                </Slider>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.contenedorCarrusel}>
+      <p className={styles.tituloCarrusel}>{titulo}</p>
+      <div className={styles.tarjetaCarrusel}>
+        <Slider {...settings}>
+          {productos
+            .filter((d) => d.categoria === titulo)
+            .map((d) => (
+              <div key={d.ID} className={styles.contenedor}>
+                <Link
+                  to={`/ProductoDetalles/${d.ID}`}
+                  className={styles.contenedorImagen}
+                >
+                  <img className={styles.imagen} src={d.img} alt={d.nombre} />
+                </Link>
+                <div className={styles.descripcion}>
+                  <p className={styles.descripcionNombre}>{d.nombre}</p>
+                  <p className={styles.descripcionPrecio}>{d.precio} $</p>
+                  <Link
+                    to={`/ProductoDetalles/${d.ID}`}
+                    className={`${styles.descripcionBoton} ${global.boton}`}
+                  >
+                    Ver Detalles
+                  </Link>
+                  <button
+                    className={`${styles.descripcionBoton} ${global.boton}`}
+                    onClick={() => handleAddToCart(d)}
+                  >
+                    Añadir Carrito
+                  </button>
+                </div>
+              </div>
+            ))}
+        </Slider>
+      </div>
+    </div>
+  );
 }
 
-
-
-export default caruselMenu;
+export default CaruselMenu;
--- src/Pages/Carrito/Carrito.jsx
+++ src/Pages/Carrito/Carrito.jsx
@@ -1,171 +1,146 @@
-import styles from './Carrito.module.css';
-import image1 from '../../assets/fondoCarrito.png';
-import logoCarrito from '../../assets/logoPedido.png'
+import styles from "./Carrito.module.css";
+import image1 from "../../assets/fondoCarrito.png";
+import logoCarrito from "../../assets/logoPedido.png";
 
-
-
-import SwitchSelector from "react-switch-selector"; 
-import { useContext, useEffect, useState } from 'react';
-import { DataContext } from '../../Context/DataProvider';
-import ProductoPedido from '../../Components/ProductoPedido/ProductoPedido'
-import { PayPalButtons } from '@paypal/react-paypal-js';
+import SwitchSelector from "react-switch-selector";
+import { useContext, useEffect, useState } from "react";
+import { DataContext } from "../../Context/DataProvider";
+import ProductoPedido from "../../Components/ProductoPedido/ProductoPedido";
+import { PayPalButtons } from "@paypal/react-paypal-js";
+import useCart from "../../hooks/useCart";
 
 function Carrito() {
+  const value = useContext(DataContext);
+  const productos = value.productos;
+  const { cart, loading } = useCart();
+  const [total, setTotal] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("Pick-up"); // Inicializar selectedOption
 
-    const value = useContext(DataContext);
-    const [carrito, setCarrito] = value.carrito;
-    const [total, setTotal] = value.total;
-    const productos = value.productos
-    const [selectedOption, setSelectedOption] = useState('');
+  useEffect(() => {
+    if (!cart || !productos) return;
 
+    let subtotal = 0;
+    cart.products.forEach((item) => {
+      const producto = productos.find((p) => p.ID === item.ID);
+      if (producto) {
+        subtotal += producto.precio * item.quantity;
+      }
+    });
 
-    useEffect(() => {
-        console.log('selectedOption:', selectedOption);
+    let shippingCost = 0;
+    if (selectedOption === "Delivery") {
+      shippingCost = 8;
+    }
+    setTotal(subtotal + shippingCost);
+  }, [cart, productos, selectedOption]);
 
-        let subtotal = 0;
-        carrito.forEach((item) => {
-            const producto = productos.find((p) => p.ID === item.ID);
-            subtotal += producto.precio;
-        });
-        let shippingCost = 0;
-        if (selectedOption === 'Delivery') {
-            shippingCost = 8;
-        }
-        setTotal(subtotal + shippingCost);
-        }, [carrito, productos, selectedOption]);
-    
-        useEffect(() => {
-        console.log('selectedOption:', selectedOption);
-        }, [selectedOption]);
-    
-        const onChange = (newValue) => {
-            setSelectedOption(newValue.value);
-            console.log(newValue)
-        };
-    
+  const onChange = (newValue) => {
+    setSelectedOption(newValue.value);
+  };
 
-    console.log(carrito)
+  const options = [
+    {
+      label: "Delivery",
+      value: "Delivery",
+      selectedBackgroundColor: "#ff8a00",
+    },
+    {
+      label: "Pick-up",
+      value: "Pick-up",
+      selectedBackgroundColor: "#fbc531",
+    },
+  ];
 
+  const updateCartState = (updatedCart) => {
+    setCart(updatedCart);
+  };
 
-    const options = [
-        {
-            label: <span>Foo</span>,
-            value: {
-            foo: true
-            },
-            selectedBackgroundColor: "#ff8a00",
-        },
-        {
-            label: "Bar",
-            value: "bar",
-            selectedBackgroundColor: "#fbc531"
-        }
-    ];
-    
-    //const onChange = (newValue) => {
-    //    console.log(newValue);
-    //};
-    
-    const initialSelectedIndex = options.findIndex(({value}) => value === "bar");
-    
-    
-    return (
-        <div className='Carrito'>
-            <div className={styles.topContainer}>
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-                <div><img className={styles.Panescss} src={image1} alt="imagen1" /></div>
-                <div className={styles.containerOverlay}></div>
-                <div className={styles.containerCaption}>
-                    <h3 className={styles.Text}>Tu pedido</h3>
-                    <div style={{ height: 70, width: 700}}>
-                    <SwitchSelector
-                        backgroundColor="#a6a6a6"
-                        border={{}}
-                        initialSelectedIndex={1}
-                        fontSize={17}
-                        name="botonseleccion"
-                        onChange={onChange}
-                        optionBorderRadius={{}}
-                        options={[
-                        {
-                            label: 'Delivery',
-                            value: 'Delivery'
-                        },
-                        {
-                            label: 'Pick-up',
-                            value: 'Pick-up'
-                        }
-                        ]}
-                    selectedBackgroundColor="#ff8a00"
-                    wrapperBorderRadius={{}}
-                    />
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.contactContainer}>
-                <div className={styles.spacer}>
-                    <div className={styles.contact}>
-                    {carrito.length >= 0 ? ( carrito.map((ID) => (
-                    <ProductoPedido key={ID} id={ID} />
-                    ))) : (
-                    <p>No items in cart</p>
-                    )}
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-
-
-
-        <div className={styles.totalPagar}>
-                <div className={styles.spacer}>
-
-                <div className={styles.allignSide}>
-
-                    <div className={styles.feedback}>
-
-                        <div className={styles.resumenPedido}>
-                            <h2>Resumen de tu pedido</h2>
-                            <p className={styles.unrDesc}>Precio Productos ----------------------- ${total - (selectedOption === 'Delivery' ? 8 : 0)}</p>
-                            <p className={styles.unrDesc}>Precio Envío ----------------------------- {selectedOption === 'Delivery' ? '$8' : 'Gratis'}</p>
-                        </div>
-
-                    </div>
-
-                    <div className={styles.subtotalContenedor}>
-                            <h3> Subtotal </h3>
-                            <div className={styles.subtotalPrecio}>
-                                <p> ${total - (selectedOption === 'Delivery' ? 8 : 0)} </p>
-                            </div>
-                    </div>
-
-                    <div className={styles.logoCarrito}>
-                            
-                                <img src={logoCarrito} alt='Carrito'></img>
-                            
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div className="Carrito">
+      <div className={styles.topContainer}>
+        <div>
+          <img className={styles.Panescss} src={image1} alt="imagen1" />
         </div>
-
-            
-            <div className={styles.sSpace}>
-                <div className={styles.paypal}>
-                <PayPalButtons />
-                </div>
-                <button className={styles.navButton} >----------Pagar----------</button>
-            </div>
-            
-            
+        <div className={styles.containerOverlay}></div>
+        <div className={styles.containerCaption}>
+          <h3 className={styles.Text}>Tu pedido</h3>
+          <div style={{ height: 70, width: 700 }}>
+            <SwitchSelector
+              backgroundColor="#a6a6a6"
+              border={{}}
+              initialSelectedIndex={1}
+              fontSize={17}
+              name="botonseleccion"
+              onChange={onChange}
+              optionBorderRadius={{}}
+              options={options}
+              selectedBackgroundColor="#ff8a00"
+              wrapperBorderRadius={{}}
+            />
+          </div>
         </div>
-        
-    );
+      </div>
+
+      <div className={styles.contactContainer}>
+        <div className={styles.spacer}>
+          <div className={styles.contact}>
+            {cart && cart.products.length > 0 ? (
+              cart.products.map((item) => (
+                <ProductoPedido
+                  key={item.ID}
+                  item={item}
+                  productos={productos}
+                  updateCartState={updateCartState}
+                />
+              ))
+            ) : (
+              <p>No items in cart</p>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.totalPagar}>
+        <div className={styles.spacer}>
+          <div className={styles.allignSide}>
+            <div className={styles.feedback}>
+              <div className={styles.resumenPedido}>
+                <h2>Resumen de tu pedido</h2>
+                <p className={styles.unrDesc}>
+                  Precio Productos ----------------------- $
+                  {total - (selectedOption === "Delivery" ? 8 : 0)}
+                </p>
+                <p className={styles.unrDesc}>
+                  Precio Envío -----------------------------{" "}
+                  {selectedOption === "Delivery" ? "$8" : "Gratis"}
+                </p>
+              </div>
+            </div>
+            <div className={styles.subtotalContenedor}>
+              <h3> Subtotal </h3>
+              <div className={styles.subtotalPrecio}>
+                <p> ${total - (selectedOption === "Delivery" ? 8 : 0)} </p>
+              </div>
+            </div>
+            <div className={styles.logoCarrito}>
+              <img src={logoCarrito} alt="Carrito"></img>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.sSpace}>
+        <div className={styles.paypal}>
+          <PayPalButtons />
+        </div>
+        <button className={styles.navButton}>----------Pagar----------</button>
+      </div>
+    </div>
+  );
 }
 
 export default Carrito;
