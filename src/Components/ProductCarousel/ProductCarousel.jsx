693a09c4bf0e71e3d474aa8d200793776677ec86
vickysaldivia <vsaldivia@correo.unimet.edu.ve>
2024-07-05 03:07:01 -0400

    ayuda

--- src/Components/ProductCarousel/ProductCarousel.jsx
+++ src/Components/ProductCarousel/ProductCarousel.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-key */
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -17,34 +16,30 @@ import image6 from '../../assets/FrappeChocolate.png';
 import image7 from '../../assets/DesayunoAmericano.png';
 import image8 from '../../assets/OmeleteDeVegetales.png';
 
-
-
-//Flechas para ver los demas productos
+// Flechas para ver los demas productos
 function SampleNextArrow(props) {
-     const { className, style, onClick } = props;
-     return (
-         <div
-             className={`${className} ${styles.arrow}`}
-             style={{ ...style, right: "10px" }}
-             onClick={onClick}
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`${className} ${styles.arrow}`}
+            style={{ ...style, right: "10px" }}
+            onClick={onClick}
         />
     );
 }
 
 function SamplePrevArrow(props) {
     const { className, style, onClick } = props;
-     return (
-         <div
-             className={`${className} ${styles.arrow}`}
-             style={{ ...style, left: "10px" }}
-             onClick={onClick}  
-              
+    return (
+        <div
+            className={`${className} ${styles.arrow}`}
+            style={{ ...style, left: "10px" }}
+            onClick={onClick}
         />
-     );
+    );
 }
 
 function ProductCarousel() {
-
     const navigate = useNavigate();
     const productos = [
         { id: 1, name: 'CAFE AMERICANO', price: '$3.5', image: image1},
@@ -56,76 +51,68 @@ function ProductCarousel() {
         { id: 7, name: 'DESAYUNO AMERICANO', price: '$8.00', image: image7 },
         { id: 8, name: 'OMELET DE VEGETALES', price: '$7.00', image: image8 },
     ];
-    
+
     var settings = {
         dots: false,
         infinite: true,
         speed: 500,
         lazyLoad: true,
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
         initialSlide: 0,
         slidesMargin: 10,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 4,
-              infinite: true,
-              dots: false
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
         ]
-      };
+    };
 
-   
     return (
-        
         <div className={styles.contenedorCarrusel}>
             <p className={styles.tituloCarrusel}>Calidad a tu alcance</p>
             <div className={styles.tarjetaCarrusel}>
                 <Slider {...settings}>
-                    {productos.map( (d) => (
-                        <div className={styles.contenedor}>
-                            
+                    {productos.map((d) => (
+                        <div className={styles.contenedor} key={d.id}>
                             <div className={styles.contenedorImagen}>
                                 <img className={styles.imagen} src={d.image} alt="opcionMenú" />
                             </div>
-
                             <div className={styles.descripcion}>
                                 <p className={styles.descripcionNombre}>{d.name} </p>
                                 <p className={styles.descripcionPrecio}>{d.price} </p>
                                 <button onClick={() => navigate('/Menu')} className={`${styles.descripcionBoton} ${global.boton}`}>Menú</button>
-                                
                             </div>
-                            
                         </div>
-
                     ))}
-
                 </Slider>
             </div>
         </div>
-
     );
 }
 
 export default ProductCarousel;
+
